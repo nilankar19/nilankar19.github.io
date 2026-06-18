18308bdec6cc714f3f83af140dca476621f016d1
Nilankar <nilankar19@gmail.com>
2026-06-18 20:45:45 +0530

    updated_footer

--- _next/static/-C74eeCjIzUvQp-bqWW4N/_buildManifest.js
+++ _next/static/-C74eeCjIzUvQp-bqWW4N/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/portfolio_//_next/:path+",
-        "destination": "/portfolio_/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
