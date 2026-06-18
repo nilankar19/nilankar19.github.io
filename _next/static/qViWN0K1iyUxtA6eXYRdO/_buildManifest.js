93cfe809b6d8bb66cdb51c2e21382004505f1a2b
Nilankar <nilankar19@gmail.com>
2026-06-18 20:39:24 +0530

    bug_fix

--- _next/static/qViWN0K1iyUxtA6eXYRdO/_buildManifest.js
+++ _next/static/qViWN0K1iyUxtA6eXYRdO/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio_//_next/:path+",
+        "destination": "/portfolio_/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
